7bfca19e05665f1b13df1cc254f4dea75968becd
FanSky_Qs <3141865879@qq.com>
2023-03-04 06:51:22 +0800

    点赞

--- apps/DianZan.js
+++ apps/DianZan.js
@@ -19,12 +19,13 @@ export class DianZan extends plugin {
     async thuMUp(e){
         if(e.isGroup){
             await Bot.pickFriend(e.user_id).thumbUp(20);
-            let MsgListTwo=[segment.at(e.user_id),"给你点赞了喵~\n没点上加我好友发【点赞】~",'\n',segment.image(urls_one)]
+            let MsgListTwo=[segment.at(e.user_id),"给你点赞了喵~\n没点上加我好友发【打卡】~",'\n',segment.image(urls_one)]
             await e.reply(MsgListTwo)
             return true
         }
         await Bot.sendLike(e.user_id,20)
-        await e.reply("赞了噢喵~,可以..可以回我一下嘛o(*////▽////*)q~,没点上请加我好友再发【点赞】~")
+        await e.reply("赞了噢喵~,可以..可以回我一下嘛o(*////▽////*)q~,没点上请加我好友再发【打卡】~")
         return true
     }
+
 }
