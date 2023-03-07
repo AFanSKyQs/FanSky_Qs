6245c434ad8beaefcf4587fb34733ed4349a2283
FanSky_Qs <3141865879@qq.com>
2023-03-08 01:29:12 +0800

    更新打卡用户统计功能

--- apps/SignIn.js
+++ apps/SignIn.js
@@ -2,20 +2,22 @@ import fs from "fs";
 import common from "../../../lib/common/common.js";
 import plugin from "../../../lib/plugins/plugin.js";
 import puppeteer from "../../../lib/puppeteer/puppeteer.js"
-import { segment } from "oicq"
+import {segment} from "oicq"
 import {getAcgBg, getBgImg, getByImg, getTuImg} from "../models/getTuImg.js";
 import {getGroup} from "../models/getGroupList.js";
 import {getWords} from "../models/getAwords.js";
 import crypto from "crypto";
-let DelPath=`${process.cwd()}/data/html/UserCard/`
+
+let DelPath = `${process.cwd()}/data/html/UserCard/`
 let _path = `${process.cwd()}/resources/FanSky`
 let path = `${process.cwd()}/resources/FanSky/SignIn.json`
 let CssPath = `${process.cwd()}/plugins/FanSky_Qs/resources/Card/`
 let path_SignTop = `${process.cwd()}/resources/FanSky/SignTop.json`
 let htmlPath = `${process.cwd()}/plugins/FanSky_Qs/resources/Card/Card.html`
 const name = '22、33'
-let Acg_url = [`https://dev.iw233.cn/api.php?sort=cat`,'https://api.gmit.vip/Api/DmImg?format=image']
+let Acg_url = [`https://dev.iw233.cn/api.php?sort=cat`, 'https://api.gmit.vip/Api/DmImg?format=image']
 let Acg_url_ = Acg_url[Math.floor(Math.random() * Acg_url.length)]
+
 export class ClockIn extends plugin {
     constructor() {
         super({
@@ -39,9 +41,49 @@ export class ClockIn extends plugin {
                 {
                     reg: /^#?(首次|最开始|第一次|第1次|第one次)(打卡|冒泡|签到|冒泡泡)时间?$/,
                     fnc: 'FirstSignTime',
-                },
+                }
+                ,
+                {
+                reg: /^#?(打卡|冒泡)(用户|统计|记录|总计)$/,
+                    fnc: 'CardList',
+                }
             ]
         })
+    }
+    async ChangePath(changePath){
+        return changePath.replace(/\\/g,"/");
+    }
+    async CardList(e){
+        const RunPath= await this.ChangePath(path);
+        let isExist = await this.isFileExist(RunPath);
+        if (!isExist) {
+            e.reply('还没有人打卡哦~')
+            return true
+        }
+        let data = JSON.parse(fs.readFileSync(RunPath));
+         let Num = 0;
+        let SignNum=0 ;
+        for (let user in data) {
+            if(data[user].today===true){
+                SignNum++;
+            }
+            data[user].today = false;
+            Num++;
+        }
+        e.reply(`今日打卡:${SignNum}人\n总打卡用户:${Num}人`)
+        return true
+    }
+
+    async isFileExist(isFilePath) {
+        return new Promise((resolve, reject) => {
+            fs.access(isFilePath, (err) => {
+                if (err) {
+                    resolve(false);
+                } else {
+                    resolve(true);
+                }
+            });
+        });
     }
     async FirstSignTime(e) {
         if (!fs.existsSync(_path)) {
@@ -63,17 +105,19 @@ export class ClockIn extends plugin {
         e.reply(`您首次打卡时间:${FirstSignTime}`)
         return true
     }
-    async setCard(e,nowCardNum){
+
+    async setCard(e, nowCardNum) {
         let nowTime = new Date(Date.now()).toLocaleString();
-        let Group=await getGroup(e)
-        for(let i=0;i < Group.length; i++){
-            await Bot.pickGroup(Group[i]).setCard(Bot.uin, name + '已打卡:' + nowCardNum + ' | 指令不用私聊噢~' )
+        let Group = await getGroup(e)
+        for (let i = 0; i < Group.length; i++) {
+            await Bot.pickGroup(Group[i]).setCard(Bot.uin, name + '已打卡:' + nowCardNum + ' | 指令不用私聊噢~')
             await common.sleep(1000)
         }
     }
+
     async SingleTest(e) {
         if (!e.isGroup) {
-            await Bot.sendLike(e.user_id,20)
+            await Bot.sendLike(e.user_id, 20)
             await e.reply("赞了噢喵~,可以..可以回我一下嘛o(*////▽////*)q~")
             return true
         }
@@ -121,9 +165,9 @@ export class ClockIn extends plugin {
             };
         }
         if (!SignTop["AllGroupTopTime"][e.user_id]) {
-                SignTop["AllGroupTopTime"][e.user_id] = {
+            SignTop["AllGroupTopTime"][e.user_id] = {
                 TopNumber: SignTop["AllGroupTopTime"].TopToday + 1,
-                };
+            };
             SignTop["AllGroupTopTime"].TopToday++;
             fs.writeFileSync(path_SignTop, JSON.stringify(SignTop));
             SignTop = JSON.parse(fs.readFileSync(path_SignTop));
@@ -131,12 +175,12 @@ export class ClockIn extends plugin {
         //判断今日是否打卡
         if (SignDay[e.user_id].today) {
             console.log("今日已打卡")
-            let addRough=0
+            let addRough = 0
             let lastTimeTemp = SignDay[e.user_id].time
             SignDay[e.user_id].count++;
             SignDay[e.user_id].time = Date.now();
             fs.writeFileSync(path, JSON.stringify(SignDay));
-            await this.MsgList(e, SignDay, lastTimeTemp, SignTop,addRough);
+            await this.MsgList(e, SignDay, lastTimeTemp, SignTop, addRough);
             return true
         }
         //判断是否是第一次打卡
@@ -149,7 +193,7 @@ export class ClockIn extends plugin {
             SignDay[e.user_id].rough = 160;
             SignDay[e.user_id].FirstSignTime = Date.now();
             SignDay[e.user_id].today = true;
-            let addRough=160
+            let addRough = 160
             fs.writeFileSync(path, JSON.stringify(SignDay));
             /**SignTop内为所有群的打卡排行榜*/
             await this.FirstList(e, SignDay, SignTop, addRough);
@@ -185,7 +229,7 @@ export class ClockIn extends plugin {
             let TempRough = await this.GetRough(SignDay[e.user_id].continuous);
             SignDay[e.user_id].rough += TempRough;
             fs.writeFileSync(path, JSON.stringify(SignDay));
-            await this.MsgList(e, SignDay, lastTimeTemp, SignTop,TempRough);
+            await this.MsgList(e, SignDay, lastTimeTemp, SignTop, TempRough);
             return true
         }
 
@@ -201,29 +245,30 @@ export class ClockIn extends plugin {
             return newDate.getTime();
         }
     }
-    async MsgList(e, Data, LastTimeTemp, SignTop,TempRough) {
-        let ImgList=await getBgImg()
-        let syc,hy,hg,name,salou,star,yuans,Txk1;
-        [syc,hy,hg,name,salou,star,yuans,Txk1]=ImgList
-        let TuPath=await getTuImg()
-        let ByImg=await getByImg()
-        let AcgBg=await getAcgBg()
+
+    async MsgList(e, Data, LastTimeTemp, SignTop, TempRough) {
+        let ImgList = await getBgImg()
+        let syc, hy, hg, name, salou, star, yuans, Txk1;
+        [syc, hy, hg, name, salou, star, yuans, Txk1] = ImgList
+        let TuPath = await getTuImg()
+        let ByImg = await getByImg()
+        let AcgBg = await getAcgBg()
         let LastTime = new Date(LastTimeTemp).toLocaleString();
-        let NowTime=new Date(Data[e.user_id].time).toLocaleString();
-        let Words =await getWords()
+        let NowTime = new Date(Data[e.user_id].time).toLocaleString();
+        let Words = await getWords()
         const UserHtml = {
-            addRough:TempRough,
-            AcgBg:AcgBg,
-            Txk1:Txk1,
-            syc:syc,
-            hy:hy,
-            hg:hg,
-            love:name,
-            salou:salou,
-            yuans:yuans,
-            ByImg:ByImg,
-            StarImg:star,
-            TuPath:TuPath,
+            addRough: TempRough,
+            AcgBg: AcgBg,
+            Txk1: Txk1,
+            syc: syc,
+            hy: hy,
+            hg: hg,
+            love: name,
+            salou: salou,
+            yuans: yuans,
+            ByImg: ByImg,
+            StarImg: star,
+            TuPath: TuPath,
             user_tips: "今日打卡成功！",
             user_id: e.user_id, //用户QQ号
             nickname: e.sender.nickname,    //用户昵称
@@ -234,44 +279,45 @@ export class ClockIn extends plugin {
             CardTime: Data[e.user_id].time,  //打卡时间戳
             CardLastTime: LastTime, //上次打卡时间
             CardNowTime: NowTime,
-            AWords:Words,  //随机一句话
+            AWords: Words,  //随机一句话
             CardTop: SignTop["AllGroupTopTime"][e.user_id].TopNumber,  //总打卡排行
             user_img: `https://q1.qlogo.cn/g?b=qq&nk=${e.user_id}&s=160`,  //用户头像
-            Acg_url:Acg_url_,  //随机图片链接
+            Acg_url: Acg_url_,  //随机图片链接
         }
-        let NowSum=SignTop["AllGroupTopTime"].TopToday
-        let MsgList=await puppeteer.screenshot("UserCard", {tplFile: htmlPath,quality:100,CssPath, UserHtml});
+        let NowSum = SignTop["AllGroupTopTime"].TopToday
+        let MsgList = await puppeteer.screenshot("UserCard", {tplFile: htmlPath, quality: 100, CssPath, UserHtml});
         await e.reply(MsgList);
         await Bot.pickFriend(e.user_id).thumbUp(20);
         // let MsgListTwo=[segment.at(e.user_id),"\n给你点赞了喵~\n没点上加我好友发【打卡】~"]
         // await e.reply(MsgListTwo)
-        await this.setCard(e,NowSum)
+        await this.setCard(e, NowSum)
         return true
     }
-    async FirstList(e, Data, SignTop,TempRough) {
-        let ImgList=await getBgImg()
-        let syc,hy,hg,name,salou,star,yuans,Txk1;
-        [syc,hy,hg,name,salou,star,yuans,Txk1]=ImgList
-        let TuPath=await getTuImg()
-        let ByImg=await getByImg()
-        let AcgBg=await getAcgBg()
+
+    async FirstList(e, Data, SignTop, TempRough) {
+        let ImgList = await getBgImg()
+        let syc, hy, hg, name, salou, star, yuans, Txk1;
+        [syc, hy, hg, name, salou, star, yuans, Txk1] = ImgList
+        let TuPath = await getTuImg()
+        let ByImg = await getByImg()
+        let AcgBg = await getAcgBg()
         // ["dayL","fire","Kingimg","name","SaLou","star","Yuan"]
         let LastTime = new Date(Data[e.user_id].time).toLocaleString()
-        let Words =await getWords()
+        let Words = await getWords()
         const UserHtml = {
-            addRough:TempRough,
-            AcgBg:AcgBg,
-            Txk1:Txk1,
-            syc:syc,
-            hy:hy,
-            hg:hg,
-            love:name,
-            salou:salou,
-            yuans:yuans,
-            ByImg:ByImg,
-            StarImg:star,
-            TuPath:TuPath,
-            user_tips:"首次打卡！奖励160原石！",
+            addRough: TempRough,
+            AcgBg: AcgBg,
+            Txk1: Txk1,
+            syc: syc,
+            hy: hy,
+            hg: hg,
+            love: name,
+            salou: salou,
+            yuans: yuans,
+            ByImg: ByImg,
+            StarImg: star,
+            TuPath: TuPath,
+            user_tips: "首次打卡！奖励160原石！",
             user_id: e.user_id, //用户QQ号
             nickname: e.sender.nickname,    //用户昵称
             CardDay: Data[e.user_id].day,   //打卡天数
@@ -280,39 +326,41 @@ export class ClockIn extends plugin {
             CardRough: Data[e.user_id].rough,  //魔晶
             CardTime: Data[e.user_id].time,  //打卡时间戳
             CardLastTime: LastTime, //上次打卡时间
-            CardNowTime:LastTime,//现在时间
-            AWords:Words,
+            CardNowTime: LastTime,//现在时间
+            AWords: Words,
             CardTop: SignTop["AllGroupTopTime"][e.user_id].TopNumber,  //总打卡排行
             user_img: `https://q1.qlogo.cn/g?b=qq&nk=${e.user_id}&s=160`,  //用户头像
-            Acg_url:Acg_url_,  //随机图片链接
+            Acg_url: Acg_url_,  //随机图片链接
         }
-        let NowSum=SignTop["AllGroupTopTime"].TopToday
-        let MsgList=await puppeteer.screenshot("UserCard", {tplFile: htmlPath,quality:100,CssPath, UserHtml});
+        let NowSum = SignTop["AllGroupTopTime"].TopToday
+        let MsgList = await puppeteer.screenshot("UserCard", {tplFile: htmlPath, quality: 100, CssPath, UserHtml});
         await e.reply(MsgList);
         await Bot.pickFriend(e.user_id).thumbUp(20);
         // let MsgListTwo=[segment.at(e.user_id),"\n给你点赞了喵~\n没点上加我好友发【打卡】~"]
         // await e.reply(MsgListTwo)
-        await this.setCard(e,NowSum)
+        await this.setCard(e, NowSum)
         return true
     }
-    async SendMsgUrl(path,e,NowSum){
+
+    async SendMsgUrl(path, e, NowSum) {
         // 获取这张图片的Md5码
         let Md5
-        await fs.readFile(path,  function (err, data) {
+        await fs.readFile(path, function (err, data) {
             if (err) return;
             Md5 = crypto.createHash('md5').update(data, 'utf8').digest('hex');
             console.log(Md5);
             Md5 = Md5.toUpperCase();
-            Md5=Md5+""
+            Md5 = Md5 + ""
         });
         await Bot.sendPrivateMsg(Bot.uin, segment.image(`file:///${path}`)).catch((err) => {
             logger.error(err)
         })
         // let MsgListTwo=[segment.at(e.user_id),`\n给你点赞喵~\n没点上加我好友发【打卡】~\nhttps://gchat.qpic.cn/gchatpic_new/0/0-0-${Md5}/0`]
         // await e.reply(MsgListTwo)
-        await this.setCard(e,NowSum)
+        await this.setCard(e, NowSum)
         return true
     }
+
     async GetRough(continuous) {
         let rough = Math.floor(Math.random() * 40 + 120);
         // 当continuous大于3时，每增加1天，原石多增加20
