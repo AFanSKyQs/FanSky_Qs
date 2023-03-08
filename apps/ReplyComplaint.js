e7a384c2cceb2e7151bf68edf78c2ff5e410aabf
FanSky_Qs <3141865879@qq.com>
2023-03-09 04:29:26 +0800

    增加发病功能，艾特机器人无消息即可，或#发病
    增加发病功能，艾特机器人无消息即可，或#发病

---
+++ apps/ReplyComplaint.js
@@ -0,0 +1,39 @@
+import {getComplaint} from '../models/getString.js'
+
+export class ReplyComplaint extends plugin {
+    constructor() {
+        super({
+            name: '发病回复',
+            dsc: '艾特机器人，不要加任何消息，回复发病文',
+            event: 'message',
+            priority: 666,
+            rule: [
+                {
+                    reg: /.*/i,
+                    fnc: 'ReplyComplaint'
+                }, {
+                    reg: /#发病/,
+                    fnc: 'Complaint'
+                },
+            ]
+        })
+    };
+
+    async Complaint(e) {
+        if (e.atBot && !e.msg) {
+            await this.ReplyComplaint(e)
+        } else if (e.msg === '#发病') {
+            await this.ReplyComplaint(e)
+        } else {
+            return false
+        }
+    }
+
+    async ReplyComplaint(e) {
+        let Name = e.sender.nickname || e.sender.card
+        let Complaint = await getComplaint()
+        let Reply = Complaint.replace(/{target_name}/g, Name)
+        await e.reply(Reply)
+        return true
+    }
+}
