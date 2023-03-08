4d825790bbacaefb15ef9478ab95b673dcab9b5e
FanSky_Qs <3141865879@qq.com>
2023-03-09 06:09:00 +0800

    添加依赖提示，修复仅载入可用apps
    添加依赖提示，修复仅载入可用apps

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
-import fs from 'node:fs'
+import fs from 'fs'
 
-// // 等待2s
-// await new Promise((resolve) => setTimeout(resolve, 2000))
-Bot.logger.info('---------^_^---------')
-Bot.logger.info(`FanSky_Qs插件初始化~`)
+Bot.logger.info('-------------^_^--------------')
+Bot.logger.info(`----FanSky_Qs插件初始化中------`)
 
 const files = fs.readdirSync('./plugins/FanSky_Qs/apps').filter(file => file.endsWith('.js'))
 let ret = []
 files.forEach((file) => {
-  ret.push(import(`./apps/${file}`))
+    ret.push(import(`./apps/${file}`))
 })
 ret = await Promise.allSettled(ret)
 let apps = {}
+let PackageError = 0
 for (let i in files) {
-  let name = files[i].replace('.js', '')
-
-  if (ret[i].status !== 'fulfilled') {
-    logger.error(`载入插件错误：${logger.red(name)}`)
-    logger.error(ret[i].reason)
-    continue
-  }
-  apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
+    let name = files[i].replace('.js', '')
+    if (ret[i].status !== 'fulfilled') {
+        logger.error(`载入插件错误：${logger.red(name)}`)
+        const regex = /Cannot find package '([^']+)'/;
+        let reason = ret[i].reason+""
+        const match = reason.match(regex);
+        if (match) {
+            const packageName = match[1];
+            logger.warn(`请运行：${logger.red(`pnpm add ${packageName} -w`)}安装依赖`)
+            PackageError++
+        }else{
+          logger.error(ret[i].reason)
+        }
+        delete apps[name];
+        continue
+    }
+    apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
 }
-export { apps }
+Bot.logger.info(`----FanSky_Qs插件载入完成------`)
+if(PackageError>0){
+  logger.warn(logger.yellow(`---报错请按提示安装依赖，否则对应功能会无效喵！------`))
+  logger.warn(logger.yellow(`---报错请按提示安装依赖，否则对应功能会无效喵！------`))
+  logger.warn(logger.yellow(`---报错请按提示安装依赖，否则对应功能会无效喵！------`))
+}
+export {apps}
