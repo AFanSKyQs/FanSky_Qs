78d9493a54f526860dfe1e364395d5ab4b6b0705
FanSky_Qs <3141865879@qq.com>
2023-03-09 13:59:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ files.forEach((file) => {
 })
 ret = await Promise.allSettled(ret)
 let apps = {}
-let PackageError = 0
+let APackageFanError = 0
 for (let i in files) {
     let name = files[i].replace('.js', '')
     if (ret[i].status !== 'fulfilled') {
         logger.error(`载入插件错误：${logger.red(name)}`)
-        const regex = /Cannot find package '([^']+)'/;
-        let reason = ret[i].reason+""
-        const match = reason.match(regex);
-        if (match) {
-            const packageName = match[1];
-            logger.warn(`请运行：${logger.red(`pnpm add ${packageName} -w`)}安装依赖`)
-            PackageError++
+        const ARegexFan = /Cannot find package '([^']+)'/;
+        let AFanReaSon = ret[i].reason+""
+        const AMatchFan = AFanReaSon.match(ARegexFan);
+        if (AMatchFan) {
+            const APackageNameY = AMatchFan[1];
+            logger.warn(`请运行：${logger.red(`pnpm add ${APackageNameY} -w`)}安装依赖`)
+            APackageFanError++
         }else{
           logger.error(ret[i].reason)
         }
@@ -31,7 +31,7 @@ for (let i in files) {
     apps[name] = ret[i].value[Object.keys(ret[i].value)[0]]
 }
 Bot.logger.info(`----FanSky_Qs插件载入完成------`)
-if(PackageError>0){
+if(APackageFanError>0){
   logger.warn(logger.yellow(`---报错请按提示安装依赖，否则对应功能会无效喵！------`))
   logger.warn(logger.yellow(`---报错请按提示安装依赖，否则对应功能会无效喵！------`))
   logger.warn(logger.yellow(`---报错请按提示安装依赖，否则对应功能会无效喵！------`))
