f13c463ee4ac5aee3dd9e77b23492dd6082ee21c
FanSky_Qs <3141865879@qq.com>
2023-03-13 22:31:07 +0800

    检查配置文件与拉取写入配置文件
    检查配置文件与拉取写入配置文件

--- apps/Teyvat/ReadTeyvatJson.js
+++ apps/Teyvat/ReadTeyvatJson.js
@@ -1,22 +1,44 @@
 import fs from "fs";
-import FirstUpdataJSON from "./TeyvatEnTry.js";
+import {isFileExist} from "../../models/isFileExist.js";
+import fetch from "node-fetch";
 
-let DATA_PATH = `${process.cwd()}/plugins/FanSky_Qs/config/TeyvatConfig/TeyvatUrlJson.json`
-
-
-/**
- * 读取需要的数组
- * @returns {Promise<any>}
- * @constructor
- */
-async function ReturnConfig() {
-
+async function ReturnConfig(DATA_PATH) {
+    console.log(DATA_PATH)
     let PATH = DATA_PATH.replace(/\\/g, "/");
+    if (!await isFileExist(DATA_PATH)) {
+        fs.writeFileSync(DATA_PATH, '{}');
+        console.info(">>>已创建TeyvatUrlJson.json配置文件");
+    }
     let DATA_JSON = JSON.parse(fs.readFileSync(PATH));
     if (!DATA_JSON["CHAR_DATA"] || !DATA_JSON["HASH_TRANS"] || !DATA_JSON["CALC_RULES"] || !DATA_JSON["RELIC_APPEND"]) {
-        await FirstUpdataJSON()
+        console.info(">>>开始写入配置项");
+        await GetJson(PATH);
+        console.info(">>>写入配置项完成");
     }
-    return await JSON.parse(fs.readFileSync(PATH))
-
+    console.info(">>>检查配置项完成");
+    return await JSON.parse(fs.readFileSync(PATH));
+}
+async function GetJson(PATH) {
+    let DATA_JSON = JSON.parse(fs.readFileSync(PATH));
+    let CHAR_DATA = await LocalUpdateJson("https://cdn.monsterx.cn/bot/gspanel/char-data.json");
+    console.info(`>>>已写入CHAR_DATA配置项 `);
+    let HASH_TRANS = await LocalUpdateJson("https://cdn.monsterx.cn/bot/gspanel/hash-trans.json");
+    console.info(`>>>已写入HASH_TRANS配置项 `);
+    let CALC_RULES = await LocalUpdateJson("https://cdn.monsterx.cn/bot/gspanel/calc-rule.json");
+    console.info(`>>>已写入CALC_RULES配置项 `);
+    let RELIC_APPEND = await LocalUpdateJson("https://cdn.monsterx.cn/bot/gspanel/relic-append.json");
+    console.info(`>>>已写入RELIC_APPEND配置项 `);
+    DATA_JSON["CHAR_DATA"] = CHAR_DATA;
+    DATA_JSON["HASH_TRANS"] = HASH_TRANS;
+    DATA_JSON["CALC_RULES"] = CALC_RULES;
+    DATA_JSON["RELIC_APPEND"] = RELIC_APPEND;
+    fs.writeFileSync(PATH, JSON.stringify(DATA_JSON));
+}
+async function LocalUpdateJson(URL) {
+    let res = await fetch(URL).catch((err) => logger.error(err))
+    if (!res) {
+        console.log(`${URL}请求失败...`)
+    }
+    return await res.json()
 }
 export default ReturnConfig
