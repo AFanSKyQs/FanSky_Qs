5fbbef6786cdf2dd0280baaae13867b329c03daf
FanSky_Qs <3141865879@qq.com>
2023-03-15 17:48:41 +0800

    圣遗物评级获取

---
+++ apps/Teyvat/GetData/getRelicRank.js
@@ -0,0 +1,14 @@
+import { RANK_MAP } from '../../../models/Teyvat/index.js'
+
+/**
+ * 圣遗物评级获取
+ * 在角色等级较低（基础数值较低）时评级可能显示为 "ERR"
+ * 注：角色等级较低时不为 "ERR" 的评分也有可能出错
+ */
+function getRelicRank (score) {
+  console.log('进入了：getRelicRank')
+  const rank = RANK_MAP.find(r => score <= r[1])
+  return rank ? rank[0] : score <= 66 ? 'ERR' : null
+}
+
+export default getRelicRank
