9826e30f040601dfe43690de025c5c8ba57ba2bc
FanSky_Qs <3141865879@qq.com>
2023-05-29 17:33:48 +0800

    将推送消息也改为防风控模式[不推送]

--- apps/MonitorTask.js
+++ apps/MonitorTask.js
@@ -64,8 +64,23 @@ export class MonitorTask extends plugin {
                 logger.info(logger.magenta('>>>已更新GithubStatic.json'))
                 let UTC_Date = Json.commit.committer.date
                 const cnTime = new Date(UTC_Date).toLocaleString('zh-CN', {timeZone: 'Asia/Shanghai', hour12: false})
+                let MsgList = [`[FanSky_Qs插件更新自动推送]\nContributors：${Json.commit.committer.name}\nDate:${cnTime}\nMessage:${Json.commit.message}\nUrl:${Json.html_url}`]
+                let acgList = []
+                    let bot = {nickname: "FanSky_Qs更新", user_id: Bot.uin}
+                    acgList.push(
+                        {
+                            message: MsgList,
+                            ...bot,
+                        },
+                    )
+                    let ForMsg= await Bot.makeForwardMsg(acgList)
+                    ForMsg.data=ForMsg.data
+                        .replace('<?xml version="1.0" encoding="utf-8"?>', '<?xml version="1.0" encoding="utf-8" ?>')
+                        .replace(/\n/g, '')
+                        .replace(/<title color="#777777" size="26">(.+?)<\/title>/g, '___')
+                        .replace(/___+/, '<title color="#777777" size="26">FanSky_Qs插件更新</title>')
                 if (Bot.uin === 2374221304) {
-                    await Bot.pickGroup(Number(755794036)).sendMsg(`[FanSky_Qs插件更新自动推送]\nContributors：${Json.commit.committer.name}\nDate:${cnTime}\nMessage:${Json.commit.message}\nUrl:${Json.html_url}`)
+                    await Bot.pickGroup(Number(755794036)).sendMsg(ForMsg)
                 }
                 let list = cfg.masterQQ
                 if (Json.commit.message.includes("[不推送]") || !Json.commit.message) {
@@ -79,7 +94,7 @@ export class MonitorTask extends plugin {
                         logger.info(logger.magenta(`Master:${list[i]}`))
                         try {
                             // 推送消息给当前主人
-                            await Bot.pickFriend(Number(list[i])).sendMsg(`[FanSky_Qs插件更新]:\nContributors：${Json.commit.committer.name}\nDate:${cnTime}\nMessage:${Json.commit.message}\nUrl:${Json.html_url}`)
+                            await Bot.pickFriend(Number(list[i])).sendMsg(ForMsg)
                             break // 推送成功后跳出循环
                         } catch (err) {
                             logger.info(`QQ号${list[i]}推送失败，已往下走~`)
