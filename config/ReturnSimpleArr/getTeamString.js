a53b77c1d784b564dec6f5ed0fbfe03f23801924
其实雨很好 <2122840028@qq.com>
2023-12-28 20:36:25 +0800

    队伍简写改为yaml文件，可自行修改(热加载)

--- config/ReturnSimpleArr/getTeamString.js
+++ config/ReturnSimpleArr/getTeamString.js
@@ -1,212 +1,24 @@
-import {ReturnArr} from "../SimpleTeamArr.js"
+import YAML from "yaml"
+import fs from "node:fs"
+
+const default_teamPath = `${process.cwd()}/plugins/FanSky_Qs/config/default_SimpleTeamArr.yaml`;
+const teamPath = `${process.cwd()}/plugins/FanSky_Qs/config/SimpleTeamArr.yaml`;
 
 export async function ReturnTeamArr(teamName) {
-  if (typeof teamName !== 'string') return []
-
-  const TeamArr = await ReturnArr()
-  const subNames = teamName.split(' ').map(subName => subName.split('/'))
-
-  try {
-    return subNames.reduce((acc, subNameArr) => {
-      const matchingTeams = subNameArr.reduce((teams, subName) => {
-        const directMatch = TeamArr[subName]
-        if (directMatch) return [...teams, directMatch]
-
-        const letters = subName.split('')
-        if (letters.length > 5) throw new Error()
-
-        const combinations = generateCombinations(letters)
-        const matching = combinations.filter(combo => {
-          const key = combo.join('')
-          return Object.keys(TeamArr).some(team => team.includes(key))
-        })
-        const matchingTeams = matching.map(combo => {
-          const key = combo.join('')
-          return TeamArr[key]
-        })
-
-        return [...teams, ...matchingTeams]
-      }, [])
-
-      return [...acc, ...matchingTeams.flat()]
-    }, [])
-  } catch (error) {
-    return { err: '可能缺少分隔符，使用例子：\n#队伍伤害(@张三)钟离，阿贝多，可莉，魈\n#队伍伤害100000000钟离，阿贝多，可莉，魈' } 
-  }
-}
-
-function generateCombinations(letters) {
-  const result = []
-  const used = new Array(letters.length).fill(false)
-
-  function helper(combination) {
-    if (combination.length === letters.length) {
-      result.push([...combination])
-      return
+    if (typeof teamName !== 'string') return [];
+    let teamlist;
+    if (!fs.existsSync(teamPath)) {
+      teamlist = YAML.parse(fs.readFileSync(default_teamPath, 'utf8'));
+    } else {
+      teamlist = YAML.parse(fs.readFileSync(teamPath, 'utf8'));
     }
-    for (let i = 0; i < letters.length; i++) {
-      if (used[i] || (i > 0 && letters[i] === letters[i - 1] && !used[i - 1])) {
-        continue
-      }
-      used[i] = true
-      combination.push(letters[i])
-      helper(combination)
-      combination.pop()
-      used[i] = false
-    }
-  }
-
-  helper([])
-  return result
+    let result = [];
+    Object.keys(teamlist).forEach(key => {
+        if (teamName === key) {
+            result = Array.isArray(teamlist[key]) ? teamlist[key] : teamlist[key].chars || [];
+        } else if (teamlist[key].alias && teamlist[key].alias.includes(teamName)) {
+            result = teamlist[key].chars || [];
+        }
+    });
+    return result;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
